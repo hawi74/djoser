647eeaf690628c27839f6064034b5fb8d5498c9c
Filip Stawicki <filip.stawicki74@gmail.com>
2019-09-05 10:02:24 +0200

    DRY

--- djoser/webauthn/serializers.py
+++ djoser/webauthn/serializers.py
@@ -3,6 +3,7 @@ from django.db import IntegrityError, transaction
 from rest_framework import serializers
 
 from djoser.conf import settings
+from djoser.serializers import UserCreateMixin
 
 from .models import CredentialOptions
 from .utils import create_challenge, create_ukey
@@ -34,31 +35,13 @@ class WebauthnSignupSerializer(serializers.ModelSerializer):
         return username
 
 
-class WebauthnCreateUserSerializer(serializers.ModelSerializer):
+class WebauthnCreateUserSerializer(UserCreateMixin, serializers.ModelSerializer):
     class Meta:
         model = User
         fields = tuple(User.REQUIRED_FIELDS) + (
             settings.LOGIN_FIELD,
             User._meta.pk.name,
         )
-
-    # TODO: those methods are exactly the same as in djoser.serializers.UserCreateSerializer
-    # maybe extract them to a common base or a mixin?
-    def create(self, validated_data):
-        try:
-            user = self.perform_create(validated_data)
-        except IntegrityError:
-            self.fail("cannot_create_user")
-
-        return user
-
-    def perform_create(self, validated_data):
-        with transaction.atomic():
-            user = User.objects.create_user(**validated_data)
-            if settings.SEND_ACTIVATION_EMAIL:
-                user.is_active = False
-                user.save(update_fields=["is_active"])
-        return user
 
 
 class WebauthnLoginSerializer(serializers.Serializer):
